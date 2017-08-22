eb2899f76c0748cd54d40d5454a3cb46aba2c05d
hamilton moore <hamlmoor@gmail.com>
2017-08-22 17:29:55 -0500

    updating mongo

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const data = require('./models/data');
 const mustacheExpress = require("mustache-express");
 
+const mongo = require("mongodb");
+const MongoClient = mongo.MongoClient;
+const dbUrl = "mongodb://localhost:27017/userDirectory";
+
 //calling routes:
 const indexroutes = require("./otherjsfiles/indexroutes");
 const profileroutes = require("./otherjsfiles/profileroutes");
@@ -16,6 +20,25 @@ app.set("view engine", "mustache");
 app.use(express.static(path.join(__dirname, "./public")));
 app.use("/", indexroutes);
 app.use("/profile", profileroutes);
+
+app.get("/home", function (req, res) {
+    MongoClient.connect(dbUrl, function (err, db) {
+        if (err) {
+            res.status(500).send(err);
+        }
+
+        let data = db.collection("data");
+
+        data.insertMany(data, function (err, savedPeople) {
+            if (err) {
+                res.status(500).send(err);
+            }
+
+            res.send(savedPeople);
+            db.close();
+        });
+    });
+});
 
 app.listen(port, function () {
     console.log(`server is running on port ${port}!`);
